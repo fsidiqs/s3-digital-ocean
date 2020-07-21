2b924a1655d39ea7c459bfaf048a8a6935838f0f
fsidiqs <fsidiqs@hotmail.co.id>
2020-07-21 13:57:55 +0700

    category icons

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ require('dotenv').config();
 const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -59,9 +60,70 @@ const multipleImgUpload = multer({
     }
 }).array('galleryImage');
 
+const iconUpload = multer({
+    storage: multerS3({
+        s3,
+        bucket: 'files-distrodakwah',
+        acl: 'public-read',
+        key: (req, file, cb) => {
+            const fileName = file.originalname;
+            const fullPath = 'category-icons/' + fileName;
+            cb(null, fullPath)
+        },
+        contentType: multerS3.AUTO_CONTENT_TYPE
+    }),
+    limits: {
+        fileSize: 2000000
+    },
+    fileFilter: (req, file, cb) => {
+        checkFileType(file, cb);
+    }
+}).single('category_icon');
+
+
 app.get('/', (req, res, next) => {
     console.log('asd')
     return 1;
+})
+
+app.post('/category-icons', (req, res) => {
+    console.log('asdasd');
+    iconUpload(req, res, (error) => {
+        if (error) {
+            console.log('errors', error);
+            res.json({ error: error });
+        } else {
+            if (req.file === undefined) {
+                console.log('Error: No File Selected!');
+                res.status(500).send({ error: "NO_FILE_SELECTED" })
+            } else {
+                const imageName = req.file.key;
+                const imageLocation = req.file.location;
+
+                res.json({
+                    image: imageName,
+                    location: imageLocation,
+                })
+            }
+        }
+    })
+})
+
+// * remove an aws bucket object 
+
+app.post('/object/remove', (req, res, next) => {
+    console.log(req.body);
+    if (!req.body.fullPath) res.status(500).send({ error: "NO_FILE" });
+    const { fullPath } = req.body
+    params = { Bucket: 'files-distrodakwah', Key: fullPath }
+    s3.deleteObject(params, (error, data) => {
+        if (error) {
+            res.status(500).send({ error: "ERROR_DELETE" })
+        } else {
+            console.log('success');
+            res.status(200).send({ message: "successfully removed object" });
+        }
+    })
 })
 
 app.post('/vendors', (req, res, next) => {
@@ -77,7 +139,7 @@ app.post('/vendors', (req, res, next) => {
                 const galleryImgLocationArray = [];
                 for (let i = 0; i < fileArray.length; i++) {
                     fileLocation = fileArray[i].location;
-                    
+
                     galleryImgLocationArray.push(fileLocation)
                 }
                 // Save the file name into database
